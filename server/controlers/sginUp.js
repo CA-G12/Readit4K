80de70e1a9abc5a755020050e45917010b5728c8
mohmmed23 <mhmmedben3@gmail.com>
2022-09-04 14:30:08 +0300

    create validtion prosses

--- server/controlers/sginUp.js
+++ server/controlers/sginUp.js
@@ -1,26 +1,27 @@
 const {userIsExist,saveNewUser}=require('../database/qeures')
 const {jwtFun}=require('../mw')
 const bcrypt = require("bcryptjs");
-
+const{signUpSchema}=require('../validate/vaildate')
 const signUp=(req,res)=>{
  const  {name,img,email,password}=req.body
- console.log(req.body);
-  userIsExist(email).then(({rows})=>{if(rows[0]){
-    res.json({msg:"email is exist"})
-  }else{
-    bcrypt.hash(password,12).then(hashPassword=>
-      saveNewUser(name,email,hashPassword,img).
-    then(({rows})=>{
-
-      jwtFun.jwtFun(rows[0],res)}).catch(err=>res.send(err))
+  const {error}=signUpSchema.validate(req.body)
+  if(!error)  {
+    userIsExist(email).then(({rows})=>{if(rows[0]){
+      res.json({msg:"email is exist"})
+    }else{
+      bcrypt.hash(password,12).then(hashPassword=>
+        saveNewUser(name,email,hashPassword,img).
+      then(({rows})=>{
+        jwtFun.jwtFun(rows[0],res)}).catch(err=>res.send(err))
+      )
+    }
+  }  
+ )
+  } else {
+    res.json({ msg: error.details[0].message, state: 'fail' });
     
-    )
   }
 
-}
-  
-  
-  )
 }
 
 module.exports=signUp
--- server/controlers/signIn.js
+++ server/controlers/signIn.js
@@ -1,24 +1,34 @@
 const {userIsExist}=require('../database/qeures')
 const bcrypt = require("bcryptjs");
 const {jwtFun}=require('../mw')
+const {signInSchema}=require('../validate/vaildate')
 const signIn=(req,res)=>{
  const  {email,password}=req.body;
- userIsExist(email).then(({rows})=>{
-  if (rows.length===0) {
-    res.json({msg:"email is not exist"})
-  }else{
 
-    bcrypt.compare(password,rows[0].password).then(result=>{
-      if(result){
-        userIsExist(email)
-        .then(({rows})=>jwtFun.jwtFun({id:rows[0].id,name:rows[0].name,img:rows[0].img},res))
-      }else{
+const {error}=signInSchema.validate(req.body)
 
-res.json({msg:"password is false"})
-      }
-    }).catch(err=>res.send(err))
-  }
- })
+if(!error){
+  userIsExist(email).then(({rows})=>{
+    if (rows.length===0) {
+      res.json({msg:"email is not exist"})
+    }else{
+  
+      bcrypt.compare(password,rows[0].password).then(result=>{
+        if(result){
+          userIsExist(email)
+          .then(({rows})=>jwtFun.jwtFun({id:rows[0].id,name:rows[0].name,img:rows[0].img},res))
+        }else{
+  
+  res.json({msg:"password is false"})
+        }
+      }).catch(err=>res.send(err))
+    }
+   })
+}else {
+  res.json({ msg: error.details[0].message, state: 'fail' });
+  
+}
+
 
  
 }
